d0a8ab2d2f0baf01abea3067a5c76c02bc00bd54
Irina-Khutsishvili <30725314+Irina-Khutsishvili@users.noreply.github.com>
2017-08-04 16:29:48 +0400

    New Commit

---
+++ Source.cpp
@@ -0,0 +1,29 @@
+﻿#include<iostream>
+#include<vector>
+#include<string>
+using namespace std;
+/*
+template<typename A>			// saTauri: 1.5 => const  + 0.5;  & + 0.5; tipi A + 0.5
+void printVec(constvector<A>& x){
+	for (auto i = 0; i < x.size(); ++i){
+		if (i % 3 == 0)cout << endl;	// tani: 2 =>piroba + 1; danarCeni + 1
+		cout << x[i] << '\t';
+	}
+	cout << endl;
+}
+template<typename A>
+void inputVec(vector<A>& x, int s){
+	A obj;
+	for (int i = 1; i <= s; ++i){
+		cin >> obj;
+		x.push_back(obj);
+	}
+}
+*/
+int main()
+{
+	vector<int> I { 1, 2, 3, 4, 5, 6, 7 };  // 0.5
+	//intVec(I);							// 0.5
+
+	
+}
